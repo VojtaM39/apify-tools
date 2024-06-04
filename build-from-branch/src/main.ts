cf2012f8d5b7a68ad549b46bc924354b5b8f1374
VojtaM39 <vojtech.maslan1@gmail.com>
2024-06-04 15:24:32 +0200

    feat(build-from-branch): version argument

--- build-from-branch/src/main.ts
+++ build-from-branch/src/main.ts
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 
 import { Actor, log } from 'apify';
-import { createGitRepoUrl, getCurrentFolder, getCurrentGitStateOrThrow, loadActorConfigOrThrow } from './utils.js';
+import { createGitRepoUrl, getCurrentFolder, getCurrentGitStateOrThrow, loadActorConfigOrThrow, parseCommandLineArguments } from './utils.js';
 import { BUILD_TAG, VERSION_NUMBER } from './constants.js';
 
 const client = Actor.apifyClient;
@@ -9,10 +9,13 @@ const client = Actor.apifyClient;
 const config = loadActorConfigOrThrow();
 const { username } = await client.user().get();
 
+const commandLineArguments = parseCommandLineArguments();
+
 const technicalActorName = `${username}/${config.name}`;
 
 const actorClient = client.actor(technicalActorName);
-const versionClient = actorClient.version(VERSION_NUMBER);
+const versionNumber = commandLineArguments.version ?? VERSION_NUMBER;
+const versionClient = actorClient.version(versionNumber);
 const versionsClient = actorClient.versions();
 
 const { remote, branch, rootPath } = await getCurrentGitStateOrThrow();
@@ -22,22 +25,22 @@ const gitRepoUrl = createGitRepoUrl(remote, branch, currentFolder);
 const versionExists = !!await versionClient.get();
 if (versionExists) {
     await versionClient.update({
-        buildTag: 'debug',
         // @ts-expect-error - will be fixed
         sourceType: 'GIT_REPO',
         gitRepoUrl,
     });
 } else {
+    const buildTag = versionNumber === '0.0' ? 'latest' : BUILD_TAG;
     await versionsClient.create({
-        buildTag: BUILD_TAG,
-        versionNumber: VERSION_NUMBER,
+        buildTag,
+        versionNumber,
         // @ts-expect-error - will be fixed
         sourceType: 'GIT_REPO',
         gitRepoUrl,
     });
 }
 
-const build = await actorClient.build(VERSION_NUMBER, { tag: BUILD_TAG });
-log.info(`The build is running: ${build.id}`);
+const build = await actorClient.build(versionNumber);
+log.info(`The build is running: ${build.id}, version: ${versionNumber}`);
 await client.build(build.id).waitForFinish();
 log.info('Actor build finished');
